53a8e2b2d81a59c4c65270519770b5debd364d82
wilcesde <60699305+wilcesde@users.noreply.github.com>
2020-03-23 15:46:41 -0500

    Add new bussines class
    Add new bussines person type and person class

---
+++ TranspCapas/Negocio/LogicaNegocioPersona.cs
@@ -0,0 +1,19 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class LogicaNegocioPersona
+    {
+        public int NegociarInsertDatosPersona(int id, string nombre, int edad)
+        {
+            AccesoMetodosCRUDpersonas acceso = new AccesoMetodosCRUDpersonas();
+
+            return acceso.InsertPersona(id, nombre, edad);
+        }
+    }
+}
---
+++ TranspCapas/Negocio/PersonType/LogicaNegocioPersonType.cs
@@ -0,0 +1,41 @@
+﻿using Modelo.PersonType;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.PersonType
+{
+    public class LogicaNegocioPersonType
+    {
+        //Insert
+        public int NegociarInsertDatosPT(int id, string nombre)
+        {
+            AccesoMetodosCRUDperson_type acceso = new AccesoMetodosCRUDperson_type();
+
+            return acceso.InsertPersonaType(id, nombre);
+        }
+
+        //List
+        public static DataTable NegociarListarDatosPT()
+        {
+            return AccesoMetodosCRUDperson_type.ListarPersonaType();
+        }
+
+        //Update
+        public int NegociarUpdateDatosPT(int idtipo, string nombretipo)
+        {
+            AccesoMetodosCRUDperson_type acceso = new AccesoMetodosCRUDperson_type();
+            return acceso.ActualizarPersonaType(idtipo, nombretipo);
+        }
+
+        //Delete
+        public int NegociarDeleteDatosPT(int idtipo)
+        {
+            AccesoMetodosCRUDperson_type acceso = new AccesoMetodosCRUDperson_type();
+            return acceso.DeletePersonaType(idtipo);
+        }
+    }
+}
